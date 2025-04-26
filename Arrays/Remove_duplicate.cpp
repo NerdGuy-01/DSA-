d02080e5f3d2ed0264822b09059d720668c17b0b
Aarish mansur <aarishmansur@gmail.com>
2025-04-26 15:43:54 +0530

    Create Remove_duplicate.cpp

---
+++ Arrays/Remove_duplicate.cpp
@@ -0,0 +1,14 @@
+// also called Two pointer approach 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i =0;
+    for(int j=1;j<n;j++){
+        if(arr[j+1]!= arr[j]){;
+        i++;
+        }
+    }
+    return i+1;
+}
+
