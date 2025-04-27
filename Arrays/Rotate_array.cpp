8b28a3540206d178d613f360630f95ebcc0cb526
Aarish mansur <aarishmansur@gmail.com>
2025-04-27 16:05:14 +0530

    Update Rotate_array.cpp

--- Arrays/Rotate_array.cpp
+++ Arrays/Rotate_array.cpp
@@ -6,3 +6,18 @@ int temp = arr[0];
  }
  arr[n-1] =  temp;
  return arr;
+
+// shift by d Places 
+d = d%n;
+int temp[d];
+for(int i =0;i<d;i++){
+    temp[i] = arr[i];
+}
+
+for(int i = d;i<n;i++){
+    arr[i-d] = arr[i];
+}
+
+for(int i = n-d;i<n;i++){
+    arr[i] = temp[i-(n-d)];
+}
