a8e2ef2651fdd0a20802121a8aadf92c7dcd1e7f
Aarish mansur <aarishmansur@gmail.com>
2025-05-04 19:47:42 +0530

    Update Longest_Subarray.cpp

--- Arrays/Longest_Subarray.cpp
+++ Arrays/Longest_Subarray.cpp
@@ -25,3 +25,23 @@ for(int i=0;i<n;i++)[
   }
 ]
 cout<<length;
+
+// Optimized Method but oonly for positive when provided 0 and negative this code will fail 
+#include<iostream>
+using namespace std;
+
+map<long long,int > preSum;
+long long sum = 0;
+int length = 0;
+for(int i =0;i,a.size();i++){
+  if(sum ==k){
+    length = max(length,i+1);
+  }
+  long long rem = sum-k;
+  if(map.find(rem)!== preSum.end()){
+     int length = i-preSum[rem];
+     length = max(length,length)
+  }
+  preSum[sum] = i;
+
+}
